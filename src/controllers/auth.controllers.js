09d757eae45bb0d15aaa6de0eb65b5b6cd8ca5da
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-10 16:02:52 +0530

    feat: add auth and project controllers boilerplate code

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -5,4 +5,53 @@ const registerUser = asyncHandler(async (req, res) => {
   console.log("registerUser");
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  console.log("registerUser");
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const resendVerificationEmail = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  verifyEmail,
+  resendVerificationEmail,
+  refreshAccessToken,
+  forgotPassword,
+  changeCurrentPassword,
+  getCurrentUser,
+};
---
+++ src/controllers/project.controllers.js
@@ -0,0 +1,62 @@
+const getProjects = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const getProjectById = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const createProject = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const addProjectToMember = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const getProjectMembers = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const updateProjectMembers = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+const deleteMember = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  console.log("registerUser");
+});
+
+export {
+  getProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+  addProjectToMember,
+  getProjectMembers,
+  updateProjectMembers,
+  updateMemberRole,
+  deleteMember,
+};
