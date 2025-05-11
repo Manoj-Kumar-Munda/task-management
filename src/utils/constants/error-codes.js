91e16fcdf2333b6f49ec78df36aef17588be6234
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-11 22:09:02 +0530

    chore: add error codes and move contstants file

---
+++ src/utils/constants/error-codes.js
@@ -0,0 +1,10 @@
+export const ERROR_CODES = {
+  AUTH: {
+    USER_EXISTS: "user_already_exists",
+    USER_NOT_REGISTERED: "user_not_registered",
+    USER_NOT_VERIFIED: "user_not_verified",
+    INVALID_CREDENTIALS: "invalid_credentials",
+    INVALID_TOKEN: "invalid_token",
+    AUTH_VALIDATION_ERROR: "auth_validation_error",
+  },
+};
