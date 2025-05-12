cd75d758bbc3b32aab3362d7759360b654cf934e
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-12 10:02:21 +0530

    feat: add user router and centralized error handling

--- src/app.js
+++ src/app.js
@@ -4,7 +4,23 @@ const app = express();
 
 //router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/users", userRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err?.statusCode || 500;
+  const message = err?.message || "Internal Server Error";
+  const errorCode = err?.code ?? "";
+  const errors = err?.errors ?? [];
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+    errorCode,
+    errors,
+  });
+});
 
 export default app;
