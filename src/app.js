15c26f642d8caebc4f2ca4cfc40d39d05a699b0b
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-13 10:28:18 +0530

    feat: configure CORS and enhance request parsing middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("/public"));
+app.use(express.json());
 //router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/auth.routes.js";
