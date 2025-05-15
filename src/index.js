3e5aad33d2eb256f00e1f5c54dae056f52a02d8d
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-15 08:46:06 +0530

    fix: env variables loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import app from "./app.js";
-import dotenv from "dotenv";
+import "./config/env.js";
 import connectDB from "./db/index.js";
-
-dotenv.config({
-  path: "./.env",
-});
+import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
 
+console.log("PORT:", process.env.PORT);
 connectDB()
   .then(() => {
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
 import fs from "fs";
 import { v2 as cloudinary } from "cloudinary";
 
