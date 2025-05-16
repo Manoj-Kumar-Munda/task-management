50021b5faaa52e163bc6b47c0836ca7e6d1a64dd
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-16 09:37:34 +0530

    feat: add resendmail verification validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, oneOf } from "express-validator";
 
 const userRegistrationValidator = () => {
   return [
@@ -16,21 +16,13 @@ const userRegistrationValidator = () => {
       .withMessage("username should be at least 3 characters long")
       .isLength({ max: 13 })
       .withMessage("username cannot exceed 13 characters"),
-    body("password")
-      .notEmpty()
-      .withMessage("Password is required"),
-    body("fullName")
-      .notEmpty()
-      .trim()
-      .withMessage("Full name is required"),
-    body("avatar.url")
-      .optional()
-      .isURL()
-      .withMessage("Avatar URL is invalid"),
+    body("password").notEmpty().withMessage("Password is required"),
+    body("fullName").notEmpty().trim().withMessage("Full name is required"),
+    body("avatar.url").optional().isURL().withMessage("Avatar URL is invalid"),
     body("avatar.localpath")
       .optional()
       .isString()
-      .withMessage("localpath is invalid")
+      .withMessage("localpath is invalid"),
   ];
 };
 
@@ -46,4 +38,29 @@ const userLoginValidator = () => {
   ];
 };
 
-export { userRegistrationValidator, userLoginValidator };
+const resendVerficationValidator = () => {
+  return [
+    oneOf([
+      body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid"),
+      body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("username is required")
+        .isLength({ min: 3 })
+        .withMessage("username should be at least 3 characters long")
+        .isLength({ max: 13 })
+        .withMessage("username cannot exceed 13 characters"),
+    ]),
+  ];
+};
+
+export {
+  userRegistrationValidator,
+  userLoginValidator,
+  resendVerficationValidator,
+};
