d1f62f87077699ae7a1fcae07d07b8f5224ab487
Manj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-05-20 09:53:52 +0530

    refactor: clean up code formatting

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,16 +1,10 @@
 import { validationResult } from "express-validator";
 import { ApiError } from "../utils/api-error.js";
-import { ERROR_CODES } from "../utils/constants/error-codes.js";
 export const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty) {
     return next();
   }
 
-  throw new ApiError(
-    422,
-    "Received data is not valid",
-    errors.array(),
-    ERROR_CODES.AUTH.AUTH_VALIDATION_ERROR,
-  );
+  throw new ApiError(422, "Received data is not valid", "", errors.array());
 };
--- src/validators/index.js
+++ src/validators/index.js
@@ -3,18 +3,18 @@ import { body, oneOf } from "express-validator";
 const userRegistrationValidator = () => {
   return [
     body("email")
-      .trim()
-      .notEmpty()
-      .withMessage("Email is required")
-      .isEmail()
+  .trim()
+  .notEmpty()
+  .withMessage("Email is required")
+  .isEmail()
       .withMessage("Email is invalid"),
     body("username")
-      .trim()
-      .notEmpty()
-      .withMessage("username is required")
-      .isLength({ min: 3 })
-      .withMessage("username should be at least 3 characters long")
-      .isLength({ max: 13 })
+  .trim()
+  .notEmpty()
+  .withMessage("username is required")
+  .isLength({ min: 3 })
+  .withMessage("username should be at least 3 characters long")
+  .isLength({ max: 13 })
       .withMessage("username cannot exceed 13 characters"),
     body("password").notEmpty().withMessage("Password is required"),
     body("fullName").notEmpty().trim().withMessage("Full name is required"),
