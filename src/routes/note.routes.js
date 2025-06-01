217935739d4507625743148de04ecfc7eee37630
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-06-01 20:37:38 +0530

    feat: refactor note routes

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -4,18 +4,37 @@ import {
   verifyToken,
 } from "../middlewares/auth.middleware.js";
 import {
+  createNote,
   deleteNote,
   getNoteById,
+  getNotes,
   updateNote,
 } from "../controllers/note.controllers.js";
 import { UserRolesEnum } from "../utils/constants/constants.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { updateNoteValidator } from "../validators/note.validators.js";
+import {
+  createNoteValidator,
+  updateNoteValidator,
+} from "../validators/note.validators.js";
 
 const router = Router();
 
 router
-  .route("/:noteId")
+  .route("/:projectId")
+  .post(
+    verifyToken,
+    validateProjectPermissions([
+      UserRolesEnum.ADMIN,
+      UserRolesEnum.PROJECT_ADMIN,
+    ]),
+    createNoteValidator(),
+    validate,
+    createNote,
+  )
+  .get(verifyToken, getNotes);
+
+router
+  .route("/:projectId/:noteId")
   .get(verifyToken, getNoteById)
   .put(
     verifyToken,
